3b00e78e8734696e18f7b1e1534e43e2ad159c3d
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2020-03-19 15:57:10 -0300

    add basic structure

---
+++ src/main/kotlin/com/freemanpivo/gestorponto/GestorpontoApplication.kt
@@ -0,0 +1,11 @@
+package com.freemanpivo.gestorponto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GestorpontoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GestorpontoApplication>(*args)
+}
---
+++ src/test/kotlin/com/freemanpivo/gestorponto/GestorpontoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.freemanpivo.gestorponto
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class GestorpontoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
