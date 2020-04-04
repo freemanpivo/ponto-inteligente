5fe94266dee788142722df2e39ea2fd396db40ac
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2020-04-04 19:09:22 -0300

    Create LancamentoDto

---
+++ src/main/kotlin/com/freemanpivo/gestorponto/dto/LancamentoDto.kt
@@ -0,0 +1,15 @@
+package com.freemanpivo.gestorponto.dto
+
+import javax.validation.constraints.NotEmpty
+
+data class LancamentoDto (
+        @get:NotEmpty(message="Campo data de registro do ponto é obrigatório.")
+        val dataRegistroPonto: String? = null,
+        @get:NotEmpty(message="Campo tipo de lançamento é obrigatório.")
+        val tipo: String? = null,
+
+        val descricao: String? = null,
+        val localizacao: String? = null,
+        val funcionarioId: String? = null,
+        val id: String? = null
+)
